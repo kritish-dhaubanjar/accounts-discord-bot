8728f4932695066660e7c4ccc997c096c1363468
Kritish Dhaubanjar <kritish.dhaubanjar@gmail.com>
2021-05-08 21:56:26 +0545

    :rocket: &account on #help

---
+++ index.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const axios = require("axios");
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+const currencyFormatter = Intl.NumberFormat("en-IN", {
+  maximumSignificantDigits: 3,
+});
+
+client.on("ready", () => {
+  console.log("strongB0x-discord-bot ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content === "&account") {
+    axios
+      .get(process.env.API, {
+        headers: {
+          Authorization: `Bearer ${process.env.AUTHORIZATION_TOKEN}`,
+        },
+      })
+      .then(({ data }) => {
+        const reply = new Discord.MessageEmbed();
+
+        reply.setTitle("Accounts & Balance");
+
+        const accounts = Object.keys(data);
+        let total = 0;
+
+        for (const account of accounts) {
+          total += data[account];
+          reply.addField(
+            account,
+            `NPR ${currencyFormatter.format(data[account])}`
+          );
+        }
+
+        reply.addField("Total", `NPR ${currencyFormatter.format(total)}`);
+
+        message.channel.send(reply);
+      });
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN);
